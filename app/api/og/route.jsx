542f81568c5126b2e6aa966a1466fa53123e6b86
dontliem1 <dontliem1@gmail.com>
2024-05-08 08:03:41 +0500

    first template version

---
+++ app/api/og/route.jsx
@@ -0,0 +1,53 @@
+import { ImageResponse } from 'next/og';
+// App router includes @vercel/og.
+// No need to install it.
+
+export async function GET(request) {
+    try {
+        const { searchParams } = new URL(request.url);
+
+        const title = searchParams.get('t')?.slice(0, 128);
+        const desc = searchParams.get('d')?.slice(0, 128);
+        const path = searchParams.get('p');
+        return new ImageResponse(
+            (
+                <div
+                    style={{
+                        fontSize: 25,
+                        lineHeight: '35px',
+                        color: '#2f2525',
+                        background: '#ffe9b2',
+                        width: '100%',
+                        height: '100%',
+                        padding: '50px 200px',
+                        textAlign: 'center',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}
+                >
+                    {path && <img
+                        width="170"
+                        height="170"
+                        src={`https://okoshki.me/storage/app/uploads/public/${path}`}
+                        style={{
+                            borderRadius: 85,
+                        }}
+                    />}
+                    <h1 style={{ fontSize: 75, lineHeight: '85px', fontWeight: 900 }}>{title}</h1>
+                    {desc}
+                </div>
+            ),
+            {
+                width: 1200,
+                height: 630,
+            },
+        );
+    } catch (e) {
+        console.log(`${e.message}`);
+        return new Response(`Failed to generate the image`, {
+            status: 500,
+        });
+    }
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <div>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          <code>app/page.js</code>
         </p>
         <div>
           <a
@@ -16,33 +13,13 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
           </a>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
+      <div>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -54,7 +31,6 @@ export default function Home() {
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -66,7 +42,6 @@ export default function Home() {
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -78,7 +53,6 @@ export default function Home() {
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
